01eb1c6505916adbd02606fbce4209741549d1ec
Luqman <luqmanxcz@gmail.com>
2022-11-08 18:18:39 -0500

    fetch weather

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 let weather = {
-  "apiKey": 
+  "apiKey": "a5a59e2f99b2037df92d3effea135978",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q="
+      + city
+      + "&units=metric&appid="
+      + this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+  },
+  displayWeather: function (data) {
+
+  }
 }
